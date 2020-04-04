b6e1918ea6709af71452f18b1148fb6f8bfa5940
mhamilton360 <mhamilton360@gmail.com>
2020-04-04 17:10:30 -0400

    Modified components to support grid layout

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -4,20 +4,24 @@ import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.css']
+  //templateUrl: './book-list.component.html',
+  templateUrl: './book-grid.component.html',
+  styleUrls: ['./book-list.component.css'],
 })
 export class BookListComponent implements OnInit {
-
+  rupeesToDollar: number = 76.43;
   books: Book[];
 
-  constructor(private _bookService: BookService) { }
+  constructor(private _bookService: BookService) {}
 
   ngOnInit(): void {
     this.listBooks();
   }
 
-  listBooks(){
-    this._bookService.getBooks().subscribe(data => this.books = data);
+  listBooks() {
+    this._bookService.getBooks().subscribe((data) => {
+      data.map(price => price.unitPrice /= this.rupeesToDollar);
+      this.books = data;
+    });
   }
 }
