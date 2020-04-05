b4e1d19bfe4ca0c8264602431437a5def4ea39da
mhamil9 <Michael.Hamilton@burlington.com>
2020-04-05 18:59:45 -0400

    Completed feature 3 search by category.

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../../common/book';
 import { BookService } from 'src/app/services/book.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
@@ -8,20 +9,33 @@ import { BookService } from 'src/app/services/book.service';
   styleUrls: ['./book-list.component.css'],
 })
 export class BookListComponent implements OnInit {
+  
   rupeesToDollar: number = 76.43;
   books: Book[];
+  currentCategoryId: number;
 
-  constructor(private _bookService: BookService) {}
+  constructor(private _bookService: BookService,
+              private _activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.listBooks();
+    this._activatedRoute.paramMap.subscribe(()=>{
+        this.listBooks();
+      })
   }
 
   listBooks() {
-    this._bookService.getBooks().subscribe((data) => {
-      data.map(item => {item.unitPrice /= this.rupeesToDollar;
-      console.log(item.imageUrl)});
-      this.books = data;
+    
+    const hasCategoryId: boolean = this._activatedRoute.snapshot.paramMap.has('id');
+    if(hasCategoryId){
+      this.currentCategoryId = +this._activatedRoute.snapshot.paramMap.get('id');
+    } else {
+      this.currentCategoryId = 1;
+    }
+
+    this._bookService.getBooks(this.currentCategoryId).subscribe(
+      data => {
+        data.map(item => item.unitPrice /= this.rupeesToDollar);
+        this.books = data;
     });
   }
 }
