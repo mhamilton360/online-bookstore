5c9ee3a5a50a24fd6e8a86cdeb38179ed4f993e7
mhamil9 <Michael.Hamilton@burlington.com>
2020-04-05 18:09:18 -0400

    Error page 404 completed

--- angular-bookstore/src/app/app.module.ts
+++ angular-bookstore/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookService } from './services/book.service';
+import { PageNotFountComponent } from './components/page-not-fount/page-not-fount.component';
+
+const routes: Routes = [
+  {path: 'books', component: BookListComponent},
+  {path: 'category/:id', component: BookListComponent},
+  {path: '', redirectTo: '/books', pathMatch: 'full'},
+  {path: '**', component: PageNotFountComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    BookListComponent,
+    PageNotFountComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     BookService
